bd247772ced49bf3f04be611f8dc77af45deccae
joelruiz2 <joelruiz2@gmail.com>
2018-07-04 03:00:50 -0500

    typo? wth that come from, this is olddd

--- bot.astar.js
+++ bot.astar.js
@@ -133,7 +133,7 @@ var astar = {
             var currentNode = openHeap.pop();
 
             // End case -- result has been found, return the traced path.
-            if (currentNode.x == endgx && currentNode.y == end.y) {
+            if (currentNode.x == end.x && currentNode.y == end.y) {
                 return pathTo(currentNode);
             }
 
